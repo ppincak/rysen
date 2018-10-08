b6eacffcaccc307ec2dc166166465d19079bada9
Unknown <peter.pincak@hotmail.com>
2018-10-08 21:09:04 +0200

    small optimization of  error handling

--- pkg/errors/error.go
+++ pkg/errors/error.go
@@ -12,12 +12,25 @@ type Error struct {
 
 // create ne error
 func NewError(message string, args ...interface{}) Error {
+	err := Error{}
+	err.Format(message, args)
+	return err
+}
+
+// create ne error with error code
+func NewErrorWithCode(message string, code string) Error {
+	return Error{
+		Message: message,
+		Code:    code,
+	}
+}
+
+// get error mesage
+func (e Error) Format(message string, args ...interface{}) {
 	if len(args) > 0 {
 		message = fmt.Sprintf(message, args)
 	}
-	return Error{
-		Message: message,
-	}
+	e.Message = message
 }
 
 // get error mesage
--- server/errors/error.go
+++ server/errors/error.go
@@ -7,32 +7,20 @@ import (
 	"github.com/ppincak/rysen/pkg/errors"
 )
 
-type ApiError struct {
-	Message string `json:"message"`
-	Key     string `json:"key"`
-}
-
-func NewApiError(message string, key string) *ApiError {
-	return &ApiError{
-		Message: message,
-		Key:     key,
-	}
-}
-
 func ErrorBadRequest(context *gin.Context, err error) {
-	var result *ApiError
+	var result errors.Error
 	if assertion, ok := err.(errors.Error); ok {
-		result = NewApiError(assertion.Message, assertion.Code)
+		result = assertion
 	} else {
-		result = NewApiError("Internal server error", "internal.server.error")
+		result = errors.NewErrorWithCode("Internal server error", "internal.server.error")
 	}
 	context.JSON(http.StatusBadRequest, result)
 }
 
 func BadRequest(context *gin.Context, err string, key string) {
-	context.JSON(http.StatusBadRequest, NewApiError(err, key))
+	context.JSON(http.StatusBadRequest, errors.NewErrorWithCode(err, key))
 }
 
 func InternalServerError(context *gin.Context, err string, key string) {
-	context.JSON(http.StatusInternalServerError, NewApiError(err, key))
+	context.JSON(http.StatusInternalServerError, errors.NewErrorWithCode(err, key))
 }
